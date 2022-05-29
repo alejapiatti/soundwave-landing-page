ea5aff97bf20a790fb02f3a5066d946e45887dcb
Ale <Aleu@gmail.com>
2022-05-29 12:05:06 +0200

    segunda

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ import Logo from '../images/logo.png';
 export default function Navbar(){
     return(
     <div calssName="Navbar">
-        <a href= "#front"><button><img src={Logo}/>Soundwave</button> alt= "soundwave</a>
-        <a href= "#segunda"><button>Discover</button> alt= "discovery"</a>
-        <a href= "#tercera"><button>Join</button> alt= "join"</a>
+        <a href= "#front"><button><img src={Logo}/>Soundwave</button></a>
+        <a href= "#segunda"><button>Discover</button></a>
+        <a href= "#tercera"><button>Join</button></a>
     </div>
     )
 
